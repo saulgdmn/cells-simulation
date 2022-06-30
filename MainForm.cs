4126b054afaa252c0ffc2e1e74af81a84b1ee1c2
Al_Sah <aplexidr@gmail.com>
2022-06-30 20:53:53 +0300

    update main form

--- MainForm.cs
+++ MainForm.cs
@@ -4,15 +4,16 @@ using System.Windows.Forms;
 using System.Threading;
 using System.Diagnostics;
 
-namespace LifeProj
-{
-    public partial class Form1 : Form
-    {
-        public Field Field;
+namespace LifeProj{
+    
+    public partial class MainForm : Form {
+        
+        public readonly Field Field;
         public double NextIterElapsed;
         public double LoopIterElapsed;
-        public Form1()
-        {
+        
+        public MainForm(){
+            
             InitializeComponent();
             SetStyle(
                 ControlStyles.AllPaintingInWmPaint | 
@@ -22,14 +23,13 @@ namespace LifeProj
             
             Field = new Field();
             Field.Init();
-            Thread thr = new Thread(ThreadFunc);
-            thr.Start();
+            
+            var thread = new Thread(ThreadFunc){IsBackground = true};
+            thread.Start();
         }
-
-        public void ThreadFunc()
-        {
-            while (true)
-            {
+        
+        private void ThreadFunc(){
+            while (true){
                 Stopwatch loopIterWatch = new Stopwatch();
                 Stopwatch nextIterWatch = new Stopwatch();
 
@@ -49,64 +49,44 @@ namespace LifeProj
             }
         }
         
-        public static void FillCircle(Graphics g, Brush brush,
-            float centerX, float centerY, float radius)
-        {
-            g.FillEllipse(brush, centerX - radius, centerY - radius,
-                radius + radius, radius + radius);
+        
+        private static void FillCircle(Graphics g, Brush brush, float centerX, float centerY, float radius){
+            g.FillEllipse(brush, centerX - radius, centerY - radius,radius + radius, radius + radius);
         }
         
-        public static void FillRectangle(Graphics g, Brush brush,
-            float centerX, float centerY, float radius)
-                {
-                    g.FillRectangle(brush, centerX - radius, centerY - radius,
-                        radius + radius, radius + radius);
-                }
+        private static void FillRectangle( Graphics g, Brush brush, float centerX, float centerY, float radius){
+            g.FillRectangle(brush, centerX - radius, centerY - radius, radius + radius, radius + radius);
+        }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
+        protected override void OnPaint(PaintEventArgs e){
+            
             base.OnPaint(e);
-
-            Graphics g = e.Graphics;
+            e.Graphics.DrawRectangle(new Pen(Color.Black), 1.0f, 1.0f, Simulation.FieldWidth, Simulation.FieldHeight);
             
-            Pen pen = new Pen(Color.Black);
-            
-            g.DrawRectangle(pen, 1.0f, 1.0f, Simulation.FieldWidth, Simulation.FieldHeight);
-            foreach (var cell in Field.Cells.ToArray())
-            {
-                if(cell == null)
+            foreach (var cell in Field.Cells.ToArray()){
+                if (cell == null){
                     continue;
-                
-                SolidBrush brush;
-                switch (cell.State)
-                {
-                    case CellState.Healthy:
-                        brush = Simulation.HealthyBrush;
-                        break;
-                    case CellState.Incubation:
-                        brush = Simulation.IncubationBrush;
-                        break;
-                    case CellState.Infected:
-                        brush = Simulation.InfectedBrush;
-                        break;
-                    case CellState.Recovered:
-                        brush = Simulation.RecoveredBrush;
-                        break;
-                    default:
-                        brush = Simulation.HealthyBrush;
-                        break;
                 }
-                FillCircle(g, brush, cell.Position.X, cell.Position.Y, (float)cell.Radius);
+                var brush = cell.State switch {
+                    CellState.Healthy => Simulation.HealthyBrush,
+                    CellState.Incubation => Simulation.IncubationBrush,
+                    CellState.Infected => Simulation.InfectedBrush,
+                    CellState.Recovered => Simulation.RecoveredBrush,
+                    _ => Simulation.HealthyBrush
+                };
+                FillCircle(e.Graphics, brush, cell.Position.X, cell.Position.Y, (float)cell.Radius);
             }
-            
-           
-            
+            DrawSimulationInformation(e.Graphics);
+        }
+
+        private void DrawSimulationInformation(Graphics graphics)
+        {
             // Set format of string.
             StringFormat drawFormat = new StringFormat();
             drawFormat.FormatFlags = StringFormatFlags.DisplayFormatControl | StringFormatFlags.FitBlackBox;
              
             // Draw string to screen.
-            e.Graphics.DrawString(
+            graphics.DrawString(
                 "NextIterElapsed: " + NextIterElapsed + "\n" + 
                 "LoopIterElapsed: " + LoopIterElapsed + "\n" + 
                 "ItersPerSecond: " + Math.Round(1000.0 / LoopIterElapsed) + "\n" +
